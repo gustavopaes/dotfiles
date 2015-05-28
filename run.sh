d44339b645c5a6a308fb69a3cd6c1de8a38908ec
Gustavo Paes <gustavo.paes@gmail.com>
2015-05-28 13:34:24 -0300

    Bugs

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@
 
 NODE_URL="http://nodejs.org/dist/v0.12.3/node-v0.12.3.tar.gz"
 SUBLIME_URL="http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb"
+GOLANG_URL="https://storage.googleapis.com/golang/go1.4.2.src.tar.gz"
 
 ## end
 if [ "$(whoami)" != "root" ]; then
@@ -32,12 +33,15 @@ apt-get -qq -y install git
 
 ## Configure GIT
 cp dotfiles/.gitconfig $HOME/.gitconfig
+chown $USER:$USER $HOME/.gitconfig
 
 ## Profile file
 cp dotfiles/.profile $HOME/.profile
+chown $USER:$USER $HOME/.profile
 
 ## Bash file
 cp dotfiles/.bashrc $HOME/.bashrc
+chown $USER:$USER $HOME/.bashrc
 
 ## SUBLIME JS
 SUBL=`whereis subl | grep subl$`
@@ -45,11 +49,11 @@ SUBL=`whereis subl | grep subl$`
 if test -z "$SUBL"; then
 	echo "Installing Sublime Text 3"
 
-	wget --directory-prefix=/tmp --output-document=sublime-text.deb $SUBLIME_URL
-	dpkg -i /tmp/sublime-text.deb
+	wget --output-document=sublime-text.deb $SUBLIME_URL
+	dpkg -i sublime-text.deb
 
 	## remove temp files
-	rm /tmp/sublime-text.deb
+	rm sublime-text.deb
 fi
 
 ## NODE JS
@@ -58,28 +62,27 @@ NODE=`whereis node | grep node$`
 if test -z "$NODE"; then
 	echo "Installing nodejs..."
 
-	wget --directory-prefix=/tmp --output-document=node.tar.gz $NODE_URL
-	tar -xf /tmp/node.tar.gz -C /tmp/node
-	cp -R /tmp/node/node* /usr/local/src/node
-	/usr/local/src/node/.configure --prefix=/usr/local/
+	wget --output-document=node.tar.gz $NODE_URL
+	tar xf node.tar.gz && rm node.tar.gz
+	
+	mv node-* /usr/local/src/node && cd /usr/local/src/node/
+
+	./configure --prefix=/usr/local/
 	make && make install
 
-	## remove temp files
-	rm /tmp/node*
-
-	exit
+	cd $ORIG
 fi
 
 ## GO LANG
 if [ ! -d "/usr/local/src/go/src" ]; then
-	mkdir "$HOME/gocode"
+	echo "Installing golang..."
 
-	cd /usr/local/src/
-	git clone https://go.googlesource.com/go
-	cd go
-	git checkout go1.4.1
-	cd src/
-	./all.bash
+	git clone https://github.com/golang/go.git /usr/local/src/go
+	cd /usr/local/src/go
+	git checkout go1.4.2
+	cd src/ && ./all.bash
+
+	ln -s /usr/local/src/go/bin/go /usr/bin/go
 fi
 
 cd $ORIG
